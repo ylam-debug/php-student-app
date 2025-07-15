bdb95538b76f3daf41ed25c3ee2ff0a584301044
Youssoupha LAM <youssouphalam@macbookpro.home>
2025-07-15 23:33:35 +0000

    Ajout docker-compose, index séparé, correction MySQL

--- src/register.php
+++ src/register.php
@@ -1,23 +1,25 @@
 <?php
-$host = 'mysql';
-$user = 'root';
-$password = 'rootpassword';
-$db = 'etudiants';
+$servername = "mysql";
+$username = "root";
+$password = "rootpassword";
+$dbname = "etudiants";
 
-$conn = new mysqli($host, $user, $password, $db);
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("Connexion échouée: " . $conn->connect_error);
+    die("Échec de la connexion : " . $conn->connect_error);
 }
 
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$email = $_POST['email'];
+$nom = $_POST['nom'] ?? '';
+$prenom = $_POST['prenom'] ?? '';
+$email = $_POST['email'] ?? '';
 
-$stmt = $conn->prepare("INSERT INTO etudiant (nom, prenom, email) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $nom, $prenom, $email);
-$stmt->execute();
+$sql = "INSERT INTO students (nom, prenom, email) VALUES ('$nom', '$prenom', '$email')";
 
-echo "Étudiant inscrit avec succès!";
-$stmt->close();
+if ($conn->query($sql) === TRUE) {
+    echo "Inscription réussie !";
+} else {
+    echo "Erreur : " . $sql . "<br>" . $conn->error;
+}
+
 $conn->close();
 ?>
